654767dcaf9f5023e942e786455802a2eae3c4c9
hee082024kh <hee082024@gmail.com>
2024-10-21 14:49:40 +0200

    Uncommented all tasks

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ class Program
         Fundament fundament = new();
 
         // Check even or odd
-        // fundament.CheckOddOrEven(3);
-        // // Display fact
-        // fundament.GetAInterestingFact("This fact is quite interesting");
-        // // Sum two numbers
-        // fundament.SumTwoNumbers();
-        // // Sum two given numbers
-        // Console.WriteLine(fundament.SumTwoNumbersOfGivenType(1, 1));
+        fundament.CheckOddOrEven(3);
+        // Display fact
+        fundament.GetAInterestingFact("This fact is quite interesting");
+        // Sum two numbers
+        fundament.SumTwoNumbers();
+        // Sum two given numbers
+        Console.WriteLine(fundament.SumTwoNumbersOfGivenType(1, 1));
         // Output fibonacci sequence
-        // fundament.PrintFibonnaciSeries(10);
+        fundament.PrintFibonnaciSeries(10);
         // Reverse the given string
         Console.WriteLine(fundament.ReverseString("Reverse this text"));
     }
